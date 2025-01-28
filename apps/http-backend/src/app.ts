0a937ed78c70f4e8c1868eda9b30e08b4fa44ebe
mnz02 <abdulminhaz47@gmail.com>
2025-01-28 19:01:55 +0530

    Configurations

--- apps/http-backend/src/app.ts
+++ apps/http-backend/src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import authRoutes from './routes/authRoutes'
+import cors from 'cors'
 
 const app: express.Application = express();
 
 
 app.use(express.json())
+app.use(cors({
+    origin: true
+}))
 
 
 app.use('/auth/v1', authRoutes)
