cd220b97a3b1637e037a0e9b5f983e3edbf9f6f6
mnz02 <abdulminhaz47@gmail.com>
2025-01-29 18:37:46 +0530

    Add zod for chats & migrations

--- apps/http-backend/src/controllers/chatController.ts
+++ apps/http-backend/src/controllers/chatController.ts
@@ -7,6 +7,11 @@ export const getChatByUserId = async (req: Request, res: Response) => {
 
         const userId = req.userId
 
+        if (!userId) {
+            res.status(401).json({ message: "Unauthorized" });
+            return
+        }
+
         const chats = await prisma.chat.findMany({
             where: { members: { some: { userId } } },
             include: { messages: true, members: true }
@@ -14,11 +19,22 @@ export const getChatByUserId = async (req: Request, res: Response) => {
 
         if (chats.length === 0) {
             res.status(404).json({ message: 'Chats not found' })
+            return
         }
 
         res.status(200).json({ chats })
     } catch (error) {
         console.error('Error getting chat by id', error)
         res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+export const createChat = async (req: Request, res: Response) => {
+    try {
+
+
+    } catch (error) {
+        console.error('Error creating chat', error)
+        res.status(500).json({ message: 'Internal server error' })
     }
 }
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -13,3 +13,17 @@ export const LoginSchema = z.object({
     username: z.string().min(3).max(20),
     password: z.string().min(8)
 })
+
+export const ChatSchema = z.object({
+    isGroup: z.boolean(),
+    groupName: z.string().optional(),
+    members: z.array(z.string().uuid()).min(1),
+    messages: z.array(
+        z.object({
+            content: z.string().min(1),
+            senderId: z.string(),
+            chatId: z.string().uuid(),
+            createdAt: z.date().optional(),
+        })
+    ).optional()
+})
