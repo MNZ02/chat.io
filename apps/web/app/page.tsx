bb0ef92613747da61cae0bb40bd6db7cf2d41df2
mnz02 <abdulminhaz47@gmail.com>
2025-02-01 03:09:38 +0530

    Homepage first commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -20,8 +20,19 @@ const ThemeImage = (props: Props) => {
 
 export default function Home() {
   return (
-    <>
+    <div>
+      <div className="flex">
 
-    </>
+        <div className="w-1/3 bg-black">
+          Hello World
+        </div>
+
+        <div className="flex flex-col justify-center items-center bg-gray-100 h-screen w-2/3">
+          <h1 className="text-3xl">Whatsapp Web</h1>
+          <h2 className="text-lg p-4">Send and receive messages without keeping your phone online.
+            Use WhatsApp on up to 4 linked devices and 1 phone at the same time.</h2>
+        </div>
+      </div>
+    </div>
   );
 }
---
+++ apps/web/app/pages/Home/MessageBox/page.tsx
@@ -0,0 +1,13 @@
+
+function page() {
+    return (
+        <div>
+            <div className="flex justify-center items-center">
+                <h1>Whatsapp Web</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default page
